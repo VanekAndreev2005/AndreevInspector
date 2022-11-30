9d0b3952805c2058e39bc505bac321d136f4ba83
VanekAndreev2005 <anda18276@gmail.com>
2022-11-30 14:07:36 +0900

    Initial commit

---
+++ AndreevInspector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InspectorLib;
+
+namespace AndreevInspectorLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp func = new FunctionInsp();
+            func.GetInspector("Васильев Василий Иванович");//Вывод главного инспектора
+            func.GetСarInspection("Автоинспекция г. Чита");//Вывод названия автоинспекции
+            func.SetInspector("");//Изменение ФИО главного инспектора
+            func.GenerateNumber(5, 'H');//Вывод госномера
+            func.GetWorker();//Вывод списка сотрудников автоинспекции
+            func.AddWorker();//Добавление сотрудника автоинспекции
+        }
+    }
+}
